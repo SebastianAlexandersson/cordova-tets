5547a35493c4b9cf308b941dcbfb9d94c6bc28cc
Sebastian Alexandersson <sebbe@sebbe.dev>
2020-05-10 22:37:54 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+OBS! Appen är testad med Android (Release name: Q, API Level: 29), ger mycket bättre resultat än senaste versionen.
+
+1. cd cordova/react-src
+2. npm install
+3. cd .. (cordova/)
+4. npm run build
+5. cordova run android --emulator
---
+++ www/precache-manifest.1b8fa46cfba43e507df063ddb07cc82e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202e8f62775d099cf9b8a5364fa44176",
+    "url": "./index.html"
+  },
+  {
+    "revision": "151512372e04117eb0fb",
+    "url": "./static/css/main.7cabb6bf.chunk.css"
+  },
+  {
+    "revision": "e01155b12479686da22c",
+    "url": "./static/js/2.bd02cf4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.bd02cf4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151512372e04117eb0fb",
+    "url": "./static/js/main.eceeb355.chunk.js"
+  },
+  {
+    "revision": "5a9c7b5121743c976941",
+    "url": "./static/js/runtime-main.139a9248.js"
+  }
+]);
